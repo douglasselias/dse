7c2d9f3d7bdcde0e6b7e5624d074f7b458c4102e
Douglas S. Elias <douglassantoselias@gmail.com>
2025-08-24 21:08:13 -0300

    Update macro name on test

--- array/array.test.c
+++ array/array.test.c
@@ -1,11 +1,7 @@
-#define DSE_ARRAY_IMPLEMENTATION
-#define DSE_ARRAY_STRIP_PREFIX
 #include "array.h"
 
 Struct(User)
 {
-  // char name[20];
-  // char email[20];
   char *name;
   char *email;
 };
@@ -15,7 +11,7 @@ void print_user(User user)
   printf("  %s, %s\n", user.name, user.email);
 }
 
-DSE_CREATE_CUSTOM_ARRAY_TYPE_FUNCTIONS(, user, User)
+DSE_DECLARE_ARRAY_FUNCTIONS(, user, User)
 
 void print_array_user(Array_user array)
 {
@@ -31,24 +27,7 @@ void print_array_user(Array_user array)
 
 int main()
 {
-  // Array array = create_array(3);
-
-  // array_append(&array, 10);
-
-  // // print_array(array);
-  // printf("Array[0]: %d\n", array.data[0]);
-
-  // destroy_array(&array);
-
   Array_user users = create_array_user((u32)-1);
-
-  // User user_one   = {"one",   "one@email.com"};
-  // User user_two   = {"two",   "two@email.com"};
-  // User user_three = {"three", "three@email.com"};
-
-  // array_append_user(&users, user_one);
-  // array_append_user(&users, user_two);
-  // array_append_user(&users, user_three);
 
   array_append_user(&users, (User){"one",   "one@email.com"});
   array_append_user(&users, (User){"two",   "two@email.com"});
@@ -58,14 +37,12 @@ int main()
   
   print_array_user(users);
 
-  // reset_array_user(&users);
   users.size = 0;
   printf("Size after reset: %d\n", users.size);
   print_array_user(users);
 
   destroy_array_user(&users);
   printf("Destroyed? %s\n", users.data == null ? "Yes" : "No");
-  // printf("Name: %s\n", users.data[0].name);
 
   return 0;
 }
